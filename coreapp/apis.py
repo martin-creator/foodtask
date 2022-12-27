6ff87971b8f8b8b844423133e853287c00dadd07
martin-creator <martinlubowa@outlook.com>
2022-12-27 10:43:05 +0300

    Added logic for getting meals

--- coreapp/apis.py
+++ coreapp/apis.py
@@ -3,7 +3,7 @@ import json
 
 from django.http import JsonResponse
 from coreapp.models import Order, OrderDetails, Restaurant, Meal
-from coreapp.serializers import RestaurantSerializer
+from coreapp.serializers import RestaurantSerializer, MealSerializer
 
 
 
@@ -18,3 +18,17 @@ def customer_get_restaurants(request):
     context={"request": request}
   ).data
   return JsonResponse({"restaurants": restaurants})
+
+def customer_get_meals(request, restaurant_id):
+  meals = MealSerializer(
+    Meal.objects.filter(restaurant_id=restaurant_id).order_by("-id"),
+    many=True,
+    context={"request": request}
+  ).data
+  return JsonResponse({"meals": meals})
+
+def customer_add_order(request, restaurant_id):
+  return JsonResponse({})
+
+def customer_get_latest_order(request, restaurant_id):
+  return JsonResponse({})
